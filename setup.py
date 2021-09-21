1eeeccf4cd012acbfdc77103efe0395394a6ed7b
risto <risto.kowaczewski@gmail.com>
2021-09-22 01:23:37 +0200

    init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='tracardi-scaffold',
-    version='1.8',
+    version='1.9',
     entry_points={
         'console_scripts': ['tps=tracardi_plugin_scaffold.create_plugin:cli'],
     },
